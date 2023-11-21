8687416633112c3c2689ad1210535974ad7ddb97
HeyyCzer <rafa.pereira.carmo@gmail.com>
2023-11-21 00:45:44 -0300

    Update install.mdx with heyy_mdt_configs snippet

---
+++ generateConfigs.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+async function run() {
+	const response = await fetch('https://cms.heyyczer.com/items/config_presets?sort=base');
+	const configs = await response.json();
+
+	for (const c of configs.data) {
+		const content = `- [${c.base}](https://cms.heyyczer.com/assets/${c.file})\n`;
+		fs.writeFileSync(path.join(__dirname, `_snippets/${c.resource}_configs.mdx`), content);
+	}
+}
+
+run();
